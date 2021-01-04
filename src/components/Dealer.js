d041a9f179938abec7408a3dc317b41b03b50d31
Michael Maton <mjmaton6@gmail.com>
2021-01-04 16:50:34 -0600

    updated css and stuff

--- src/components/Dealer.js
+++ src/components/Dealer.js
@@ -3,11 +3,12 @@ import { v4 as uuid } from "uuid";
 
 export default function Dealer(props) {
   return (
-    <div>
-      <h3>Dealer's cards:</h3>
+    <div className="cards">
+      {/* <h3>Dealer's cards:</h3> */}
       {props.cards.map((item) => {
         return(<img key={uuid()} src={item.image} alt={item.image} />);
       })}
+      {/* <p>Total: {props.total}</p> */}
     </div>
   );
 }
